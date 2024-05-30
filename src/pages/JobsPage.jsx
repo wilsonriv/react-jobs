9fc22fd821c64e2d88f26a58958919da3ed5c99e
wilson-wm <wrivera@westmonroe.com>
2024-05-30 14:09:04 -0400

    first commit

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import JobListings from '../components/JobListings';
+
+const JobsPage = () => {
+    return (
+        <section>
+            <JobListings className="bg-blue-50 px-4 py-6" />
+        </section>
+    )
+}
+
+export default JobsPage
